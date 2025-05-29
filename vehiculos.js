821d709e34385d60040695ba4ed45597b085fe8b
Pau Puigcerver <pauetxp@gmail.com>
2025-05-29 17:11:30 +0200

    Ultima versión

--- vehiculos.js
+++ vehiculos.js
@@ -1,59 +1,5 @@
 let datosjs = [
 {
-"matricula": "ABC1234",
-"color": "Rojo",
-"modelo": "Ford Fiesta",
-"fechaMatriculacion": "2022-01-15"
-},
-{
-"matricula": "GHI9012",
-"color": "Blanco",
-"modelo": "Honda Civic",
-"fechaMatriculacion": "2021-11-01"
-},
-{
-"matricula": "DEF5678",
-"color": "Azul",
-"modelo": "Toyota Corolla",
-"fechaMatriculacion": "2023-03-20"
-},
-{
-"matricula": "JKL3456",
-"color": "Negro",
-"modelo": "BMW X5",
-"fechaMatriculacion": "2024-05-01"
-},
-{
-"matricula": "MNO7890",
-"color": "Gris",
-"modelo": "Audi A4",
-"fechaMatriculacion": "2022-08-10"
-},
-{
-"matricula": "VWX9012",
-"color": "Naranja",
-"modelo": "Seat León",
-"fechaMatriculacion": "2024-02-14"
-},
-{
-"matricula": "PQR1234",
-"color": "Verde",
-"modelo": "Volkswagen Golf",
-"fechaMatriculacion": "2023-06-25"
-},
-{
-"matricula": "STU5678",
-"color": "Amarillo",
-"modelo": "Peugeot 208",
-"fechaMatriculacion": "2020-12-05"
-},
-{
-"matricula": "YZA2345",
-"color": "Blanco",
-"modelo": "Renault Clio",
-"fechaMatriculacion": "2021-07-01"
-},
-{
 "matricula": "BCD6789",
 "color": "Rojo",
 "modelo": "Fiat 500",
